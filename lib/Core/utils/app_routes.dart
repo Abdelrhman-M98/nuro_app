13492e4ba517a39636ed6d3b854bd1912e075a6a
Abdelrahman Mahmoud <abdelrhmanmahmo0ud@gmail.com>
2025-03-02 04:25:32 +0200

    add screen util package , svg ,go routes finish splash screen create login page

---
+++ lib/Core/utils/app_routes.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:neuro_app/Features/Login_View/presentation/view/login_view.dart';
+import 'package:neuro_app/Features/Splash_View/presentation/splash_view.dart';
+
+abstract class AppRouter {
+  static const kLoginView = '/loginView';
+  static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
+
+  static final router = GoRouter(
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => SplashView()),
+      GoRoute(
+        path: AppRouter.kLoginView,
+        pageBuilder: (context, state) {
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: LoginView(),
+            transitionDuration: const Duration(milliseconds: 600),
+            transitionsBuilder: (
+              context,
+              animation,
+              secondaryAnimation,
+              child,
+            ) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+          );
+        },
+      ),
+      // GoRoute(
+      //   path: kBookDetailsView,
+      //   builder: (context, state) => BlocProvider(
+      //     create: (context) => SimilarBooksCubit(getIt.get<HomeRepoImpl>()),
+      //     child: BookDetailsView(
+      //       book: state.extra as BookModel,
+      //     ),
+      //   ),
+      // ),
+      // GoRoute(
+      //   path: kSearchView,
+      //   builder: (context, state) => SearchView(),
+      // ),
+    ],
+  );
+}
---
+++ lib/Features/Splash_View/presentation/widget/splash_view_body.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:neuro_app/Core/utils/app_assets.dart';
+import 'package:neuro_app/Core/utils/app_routes.dart';
+import 'package:neuro_app/Core/utils/const.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlideAnimation();
+    navigate();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: kPrimaryColor,
+      child: Center(
+        child: AnimatedBuilder(
+          animation: animationController,
+          builder: (context, child) {
+            return SlideTransition(position: slideAnimation, child: child);
+          },
+          child: SvgPicture.asset(
+            AppAssets.kSplashImage,
+            width: 200.w,
+            height: 200.h,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void initSlideAnimation() {
+    animationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+    );
+    animationController.forward();
+  }
+
+  void navigate() {
+    Future.delayed(const Duration(seconds: 2), () {
+      GoRouter.of(context).push(AppRouter.kLoginView);
+    });
+  }
+}
